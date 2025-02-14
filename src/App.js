b07c857ab578eb7b22bf4dc45e226de6812a4bb9
Tino250 <146366795+Tino250@users.noreply.github.com>
2025-02-14 00:52:32 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path='/home'  element={<Home />}/>
+            <Route path='/'  element={<Home />}/>
             <Route path='/fluff' element={<Fluff />} />
             <Route path='/calendar' element={<Calendar />} />
             <Route path='/fluffers' element={<Fluffers />} />
